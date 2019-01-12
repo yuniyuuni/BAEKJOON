341853a78f3f130aef6f8c271c978214041496a1
yuniyuuni <thousandfour89@gmail.com>
2019-01-12 22:05:58 +0900

    A-B Create

---
+++ src/stage01/No1001.java
@@ -0,0 +1,20 @@
+package stage01;
+
+import java.util.Scanner;
+
+public class No1001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if (a>0 && b<10) {
+			System.out.println(a-b);
+		}
+
+	}
+
+}
