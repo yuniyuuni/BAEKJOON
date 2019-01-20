5411e3d7a052981e2f87e1d1a0b4e60438e05ec2
yuniyuuni <thousandfour89@gmail.com>
2019-01-20 23:19:41 +0900

    Revert "Revert "Hello World""
    This reverts commit 6ec2e09f46011f4e24269131405102ff27f6e7bd.

---
+++ src/stage01/No7287.java
@@ -0,0 +1,12 @@
+package stage01;
+
+public class No7287 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("3\nthousandfour");
+
+	}
+
+}
