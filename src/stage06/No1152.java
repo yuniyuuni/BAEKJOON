12c619f9c379268032467a84da0eda91e03df314
yuniyuuni <thousandfour89@gmail.com>
2019-02-16 18:36:28 +0900

    wrong

---
+++ src/stage06/No1152.java
@@ -0,0 +1,21 @@
+package stage06;
+
+import java.util.Scanner;
+
+public class No1152 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		String str = input.trim();
+		String[] split = str.split(" ");
+
+		System.out.println(split.length);
+
+		sc.close();
+
+	}
+
+}
