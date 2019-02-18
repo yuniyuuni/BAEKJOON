2eed2c27ed34ce95b9ad4c85a9e3767674c79c73
yuniyuuni <thousandfour89@gmail.com>
2019-02-18 13:35:59 +0900

    pass, 예외처리 추가

--- src/stage06/No1152.java
+++ src/stage06/No1152.java
@@ -9,10 +9,14 @@ public class No1152 {
 
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
-		String str = input.trim();
-		String[] split = str.split(" ");
-
-		System.out.println(split.length);
+		String str = input.trim(); //앞뒤 공백없애기
+		String[] split = str.split(" "); //공백으로 구분
+		
+		if(str.isEmpty())    //바로 엔터쳤을때 예외처리없어서 틀렸다고 떴음
+			System.out.println(0);
+		
+		else
+			System.out.println(split.length);
 
 		sc.close();
 
