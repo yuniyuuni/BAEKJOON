1dbb569cd74aa7e3f1f05c0002c6e16711d96f09
yuniyuuni <thousandfour89@gmail.com>
2019-02-18 14:17:53 +0900

    런타임에러, 배열 인덱스를 잘못 참조

--- src/stage07/No2941.java
+++ src/stage07/No2941.java
@@ -12,24 +12,35 @@ public class No2941 {
 		String[] arr = new String[str.length()];
 		arr = str.split("");
 		int count = 0;
+		
+		//char[] word = sc.nextLine().toCharArray();  세준씨코드 중, 쪼개서 넣어줌  
 
 		for (int i = 0; i < str.length(); i++) {
-			if (arr[i] == "c" && arr[i + 1] == "=")
+			if (arr[i].equals("c") && arr[i+1].equals("=")) { //arr[i] == "c" : 주소비교
 				i++;
-			else if (arr[i] == "c" && arr[i + 1] == "-")
+				//continue; for문 처음으로 감, 카운트 해줘야해서 필요없음
+			}
+			else if (arr[i].equals("c") && arr[i+1].equals("-")) {
 				i++;
-			else if (arr[i] == "d" && arr[i + 1] == "z" && arr[i + 2] == "=") {
+			}
+			else if (arr[i].equals("d") && arr[i+1].equals("z") && arr[i+2].equals("=")) {
 				i += 2;
-			} else if (arr[i] == "d" && arr[i + 1] == "-")
+			} 
+			else if (arr[i].equals("d") && arr[i+1].equals("-")) {
 				i++;
-			else if (arr[i] == "l" && arr[i + 1] == "j")
+			}
+			else if (arr[i].equals("l") && arr[i+1].equals("j")) {
 				i++;
-			else if (arr[i] == "n" && arr[i + 1] == "j")
+			}
+			else if (arr[i].equals("n") && arr[i+1].equals("j")) {
 				i++;
-			else if (arr[i] == "s" && arr[i + 1] == "=")
+			}
+			else if (arr[i].equals("s") && arr[i+1].equals("=")) {
 				i++;
-			else if (arr[i] == "z" && arr[i + 1] == "=")
+			}
+			else if (arr[i].equals("z") && arr[i+1].equals("=")) {
 				i++;
+			}
 			
 			count++;
 		}
