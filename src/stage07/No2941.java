7ba929f4f39e79da7abd45deeedd30f6a4675e55
yuniyuuni <thousandfour89@gmail.com>
2019-02-18 14:28:01 +0900

    pass

--- src/stage07/No2941.java
+++ src/stage07/No2941.java
@@ -12,36 +12,36 @@ public class No2941 {
 		String[] arr = new String[str.length()];
 		arr = str.split("");
 		int count = 0;
-		
-		//char[] word = sc.nextLine().toCharArray();  세준씨코드 중, 쪼개서 넣어줌  
+
+		// char[] word = sc.nextLine().toCharArray(); 세준씨코드 중, 쪼개서 넣어줌
 
 		for (int i = 0; i < str.length(); i++) {
-			if (arr[i].equals("c") && arr[i+1].equals("=")) { //arr[i] == "c" : 주소비교
-				i++;
-				//continue; for문 처음으로 감, 카운트 해줘야해서 필요없음
+
+			if (i < str.length() - 1) {
+				if (arr[i].equals("c") && arr[i + 1].equals("=")) { // arr[i] == "c" : 주소비교
+					i++;
+					// continue; for문 처음으로 감, 카운트 해줘야해서 필요없음
+				} else if (arr[i].equals("c") && arr[i + 1].equals("-")) {
+					i++;
+				} else if (arr[i].equals("d") && arr[i + 1].equals("-")) {
+					i++;
+				} else if (arr[i].equals("l") && arr[i + 1].equals("j")) {
+					i++;
+				} else if (arr[i].equals("n") && arr[i + 1].equals("j")) {
+					i++;
+				} else if (arr[i].equals("s") && arr[i + 1].equals("=")) {
+					i++;
+				} else if (arr[i].equals("z") && arr[i + 1].equals("=")) {
+					i++;
+				}
 			}
-			else if (arr[i].equals("c") && arr[i+1].equals("-")) {
-				i++;
+
+			if (i < str.length() - 2) {
+				if (arr[i].equals("d") && arr[i + 1].equals("z") && arr[i + 2].equals("=")) {
+					i += 2;
+				}
 			}
-			else if (arr[i].equals("d") && arr[i+1].equals("z") && arr[i+2].equals("=")) {
-				i += 2;
-			} 
-			else if (arr[i].equals("d") && arr[i+1].equals("-")) {
-				i++;
-			}
-			else if (arr[i].equals("l") && arr[i+1].equals("j")) {
-				i++;
-			}
-			else if (arr[i].equals("n") && arr[i+1].equals("j")) {
-				i++;
-			}
-			else if (arr[i].equals("s") && arr[i+1].equals("=")) {
-				i++;
-			}
-			else if (arr[i].equals("z") && arr[i+1].equals("=")) {
-				i++;
-			}
-			
+
 			count++;
 		}
 		sc.close();
